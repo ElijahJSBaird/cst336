5da5af962fe7507da40a34de4ec0f6dbbc76e1f2
Elijah Baird <ebaird@csumb.edu>
2018-08-29 22:04:57 +0000

    adding lab 1 files

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
-<html><body>
-<?php
-// A simple web site in Cloud9 that runs through Apache
-// Press the 'Run' button on the top to start the web server,
-// then click the URL that is emitted to the Output tab of the console
-
-echo " <h1> Elijah Baird's CST336 site </h1>";
-
-?>
-</body>
-</html>
+<!DOCTYPE html>
+<html>
+ 
+ <head>
+     <title>Elijah's Web Portfolio</title>
+ </head>   
+ <body>
+ <?php
+ // A simple web site in Cloud9 that runs through Apache
+ // Press the 'Run' button on the top to start the web server,
+ // then click the URL that is emitted to the Output tab of the console
+ echo " <h1> Elijah Baird's CST336 site </h1>";
+ 
+ ?>
+ </body>
+ </html>
