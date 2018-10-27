ca97e4ac4a67b94b62a768a4925ea0e5dc2de224
Elijah Baird <ebaird@csumb.edu>
2018-10-27 08:24:39 +0000

    removed results from page when first opened since nothing would have been searched for yet in lab 6

--- labs/lab6/index.php
+++ labs/lab6/index.php
@@ -132,7 +132,20 @@ function filterProducts() {
         ?>
         
     
-
+        <footer id = layoutDiv>
+            <hr id="hr100"/>
+            <small>
+            CST 336 2018 &copy; Baird <br/>
+            
+            <strong>Disclaimer:</strong>
+            The information displayed on this page is fake and is for academic purposes only. <br/>
+            <br/>
+            <img src="../../img/csumb_logo.jpg" alt="CSUMB logo" title="This is
+            the CSUMB logo"/>
+            <img src="../../img/buddy_verified.png" alt="Buddy Verified" 
+            title="This is the buddy verification badge"/>
+            </small>
+        </footer>
 
     </body>
 </html>
