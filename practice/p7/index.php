54a9a768e3dbdac0583d1c24a3f19bfbcd4d96b1
Elijah Baird <ebaird@csumb.edu>
2018-11-07 19:15:24 +0000

    added more lab 7 and lab 8

---
+++ practice/p7/index.php
@@ -0,0 +1,26 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        
+    </head>
+    <body>
+        <style type="text/css">
+            body
+            {
+                text-align: center;
+            }
+            #title
+            {
+                font-size:2em;
+                padding: 40px;
+            }
+        </style>
+        <div id="title">Famous Quote Finder</div>
+        Enter Quote Keyword <input type="text" name="keyword"/>
+    </body>
+</html>
